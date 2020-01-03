18f993877a735ff93b69477904fb98811212bee9
Justas Palumickas <jpalumickas@gmail.com>
2020-01-03 12:22:48 +0200

    Update test

--- test/propsAndCss.test.js
+++ test/propsAndCss.test.js
@@ -1,6 +1,6 @@
 import propsAndCssResolver from '../src/propsAndCss'
 
-const propsAndCss = (props) => propsAndCssResolver(props, { applyResolvers: (it) => it });
+const propsAndCss = (props) => propsAndCssResolver(props, { applyResolvers: (it) => ({ css: it }) });
 
 it('returns correct props and css', () => {
   const { css, props } = propsAndCss({ display: 'block', color: 'red', test: 'me' });
